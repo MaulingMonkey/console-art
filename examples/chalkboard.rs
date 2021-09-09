312829d97eeef20c9eeed98bdd4f03c5201aa3d8
MaulingMonkey <git@maulingmonkey.com>
2021-09-09 13:02:29 -0700

    examples: squelch !windows warnings

--- examples/chalkboard.rs
+++ examples/chalkboard.rs
@@ -1,8 +1,10 @@
 use maulingmonkey_console_art::*;
-use maulingmonkey_console_winapi_wrappers::*;
 
 fn main() {
-    #[cfg(windows)] let _ = change_console_mode(&mut std::io::stdout(), |m| m | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    #[cfg(windows)] {
+        use maulingmonkey_console_winapi_wrappers::*;
+        let _ = change_console_mode(&mut std::io::stdout(), |m| m | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    }
     let image = Image::try_from_png(std::io::Cursor::new(include_bytes!("chalkboard.png"))).unwrap();
     print!("{}", image.display_basic_24bpp_halfblocks(0));
 }
